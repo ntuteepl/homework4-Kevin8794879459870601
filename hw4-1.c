d567b62dd81e6306bd3914084e8f43da1dd003a2
Kevin8794879459870601 <146801042+Kevin8794879459870601@users.noreply.github.com>
2023-10-25 18:40:29 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+#define ARRAYSIZE 50
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    int input[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&input[i]);
+    }
+
+    bool isPrime  = true;
+    for(j=0;j<n;j++){
+        if(input[j]<=1){
+                isPrime=false;
+
+        }
+        else{
+            for(int i=2;i*i<=input[j];i++){
+                if(input[j]%i==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+
+        }
+        if(isPrime){
+            printf("YES ");
+
+        }
+        else{
+            printf("NO ");
+        }
+        isPrime=true;
+    }
+
+    }
+
 
