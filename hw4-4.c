c3fa38242f2cc1186974ffb7bc9b6e53255dcf5c
Kevin8794879459870601 <146801042+Kevin8794879459870601@users.noreply.github.com>
2023-10-30 21:26:30 +0800

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -39,12 +39,7 @@ int main(){
         }
 
         
-        //for(int i=0; i<n; i++){
-            //for(int j=0; j<n; j++){
-                //printf("%d ", a[i][j]);
-            //}
-            //printf("\n");
-        //}
+      
     }
 
     return 0;
